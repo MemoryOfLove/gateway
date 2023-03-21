d8250c9ed0a00269ab5766f0f8b0abfaa1ab25ce
yuki <554737661@qq.com>
2023-03-21 08:24:38 +0800

    初始化提交

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { VersioningType, VERSION_NEUTRAL } from '@nestjs/common';
+import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
+import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { generateDocument } from './doc';
+declare const module: any;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
+
+  // 接口版本化管理
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: [VERSION_NEUTRAL],
+  });
+  //统一错误处理
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
+  //统一相应格式
+  app.useGlobalInterceptors(new TransformInterceptor());
+  //创建文档
+  generateDocument(app)
+  //热更新
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+  await app.listen(3000);
+}
+bootstrap();
